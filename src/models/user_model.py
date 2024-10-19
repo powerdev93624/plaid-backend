84a5d52a1686d9d8d798564eab5c0f44e20a2890
powerdev93624 <michael1993624@gmail.com>
2024-10-19 13:12:18 +0300

    changed requirements.txt

--- src/models/user_model.py
+++ src/models/user_model.py
@@ -6,7 +6,7 @@ class User(db.Model):
     id = db.Column(db.String(50), primary_key = True, unique=True)
     email = db.Column(db.String(70), unique = True)
     password = db.Column(db.String(80))
-    chathistory = db.Column(db.String(4294967295))
+    chathistory = db.Column(db.String(429496729))
     plaid_access_key = db.Column(db.String(255))
     plaid_item_id = db.Column(db.String(255))
     plaid_data = db.Column(db.String(4294967295))
